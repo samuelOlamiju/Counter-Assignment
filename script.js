1070e07984aabae83e078b675b9be6995f124838
Olamiju, Oluwaseun S. <105955354+samuelOlamiju@users.noreply.github.com>
2026-05-03 23:15:57 +0100

    laptop ti fe ku

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 'use strict'
 
+let valueE1 = 0;
+let value = document.querySelector('#value');
+
+
+
+document.querySelector(".increase").addEventListener('click', function(){
+    valueE1++;
+    value.textContent = valueE1;
+    if (valueE1 === 0){
+    value.style.color = "black";
+}
+    valueE1 > 0 ? value.style.color = "green" : value.style.color = "red";
+    
+})
+
+document.querySelector(".decrease").addEventListener('click', function(){
+    valueE1--;
+    value.textContent = valueE1;
+    if (valueE1 === 0){
+    value.style.color = "black";
+}
+    valueE1 < 0 ? value.style.color = "red" : value.style.color = "green";
+})
+
+document.querySelector(".reset").addEventListener('click', function(){
+    location.reload();
+})
